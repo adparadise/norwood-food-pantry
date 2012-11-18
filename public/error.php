620f836dedd8053373906c7ce07a7fa5bbf1a5ce
Andrew Paradise <adparadise@gmail.com>
2012-11-18 09:35:53 -0500

    supporting 404's with php extension removal

--- public/error.php
+++ public/error.php
@@ -7,10 +7,17 @@
          include("includes/masthead.php"); 
       ?>
       <div class="main">
-        <h1>The Ecumenical Community Food Pantry of Norwood</h1>
 
-        <h2>Page Not Found</h2>
-        
+        <?php 
+           $message = "Page not found";
+           switch ($_SERVER['REDIRECT_STATUS']) { 
+               case "500":
+                   $message = "There was a server error...";
+                   break;
+           }
+        ?>
+        <h1><?php echo $message ?></h1>
+
         <?php include("includes/address.php"); ?>
       </div>
     </div>
