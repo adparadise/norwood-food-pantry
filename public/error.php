3863005ddf6aa7a216cd3d0d3df7e30353caea5c
Andrew Paradise <adparadise@gmail.com>
2012-11-18 18:29:58 -0500

    remove cleverness from error.php

--- public/error.php
+++ public/error.php
@@ -8,15 +8,7 @@
       ?>
       <div class="main">
 
-        <?php 
-           $message = "Page not found";
-           switch ($_SERVER['REDIRECT_STATUS']) { 
-               case "500":
-                   $message = "There was a server error...";
-                   break;
-           }
-        ?>
-        <h1><?php echo $message ?></h1>
+        <h1>Page Not Found</h1>
 
         <?php include("includes/address.php"); ?>
       </div>
